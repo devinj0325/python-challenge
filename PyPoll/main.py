6616658525d677b547baa27da8f5663d6b0195d6
Devin <djwilson@vcu.edu>
2019-08-05 18:18:17 -0400

    updated pypoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -19,11 +19,12 @@ with open(csvpath, newline='') as csvfile:
         #Vote counter
         Vote_total += 1
         #list
+        #my_list.append(object)
         if(row[2] not in candidate):
             candidate.append(row[2])
-            CountVote.append(0)
-        candidateIndex = candidate.index(row[2])
-        CountVote[candidateIndex] += 1
+            CountVote.append(0) #keep line below in if statement - but dont indent
+        candidateIndex = candidate.index(row[2]) #where candidate row starts
+        CountVote[candidateIndex] += 1 #count from 1 
     #Print
     print(f"\nElection Results\n--------------------")
     print(f"Total votes: {Vote_total}")
@@ -35,6 +36,7 @@ with open(csvpath, newline='') as csvfile:
         if (winnerCountVote<CountVote[x]):
             winnerCountVote = CountVote[x]
             winner = candidate[x]
+            #print(winner)
     
     print("--------------------------")
     print(f"Winner: {winner}")
@@ -51,7 +53,7 @@ file.write("\n------------------------")
 for x in range(len(candidate)):
     votePercent = round((CountVote [x] /Vote_total)*100,3)
     file.write("\n" + str(candidate [x] ) +" : " + str(votePercent) 
-                + "% ("+ str(CountVote [x] ) + ")")
+        + "% ("+ str(CountVote [x] ) + ")")
 file.write("\n-------------------------")
 file.write("\nWinner: " + str(winner))
 file.write("\n-------------------------")
