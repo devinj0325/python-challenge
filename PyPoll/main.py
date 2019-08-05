12fa9529dcdf8b562bef3e56d574069331b2a429
Devin <djwilson@vcu.edu>
2019-08-05 18:26:20 -0400

    final pypoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,10 +1,17 @@
+#The total number of votes cast
+#A complete list of candidates who received votes
+#The percentage of votes each candidate won
+#The total number of votes each candidate won
+#The winner of the election based on popular vote.
+
 #import
 import os
 import csv
 
 csvpath = os.path.join("election_data.csv")
 
-#variables
+#variables - total votes, candidate (list), count # of votes, 
+# winner total votes
 Vote_total = 0
 candidate = []
 CountVote = []
@@ -21,7 +28,7 @@ with open(csvpath, newline='') as csvfile:
         #list
         #my_list.append(object)
         if(row[2] not in candidate):
-            candidate.append(row[2])
+            candidate.append(row[2]) #row 2 - not header
             CountVote.append(0) #keep line below in if statement - but dont indent
         candidateIndex = candidate.index(row[2]) #where candidate row starts
         CountVote[candidateIndex] += 1 #count from 1 
@@ -30,9 +37,10 @@ with open(csvpath, newline='') as csvfile:
     print(f"Total votes: {Vote_total}")
     print("-----------------------")
     
+    #count votes
     for x in range(len(candidate)): #use round & len for amount of candidates
         votePercent = round((CountVote[x]/Vote_total) * 100)
-        print(f"{candidate[x]}: {votePercent}% ({CountVote[x]})")
+        print(f"{candidate[x]}: {votePercent}% ({CountVote[x]})") #f string
         if (winnerCountVote<CountVote[x]):
             winnerCountVote = CountVote[x]
             winner = candidate[x]
@@ -49,7 +57,7 @@ file.write("Election Results")
 file.write("\n------------------------")
 file.write("\nTotal votes:" + str(Vote_total))
 file.write("\n------------------------")
-    
+# "%"
 for x in range(len(candidate)):
     votePercent = round((CountVote [x] /Vote_total)*100)
     file.write("\n" + str(candidate [x] ) +" : " + str(votePercent) 
